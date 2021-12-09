76906ee8813b701db40620648c2fa04dc8b19ea3
Mechetel <dima.homa5@gmail.com>
2021-12-09 17:01:43 +0200

    fix: checking if user is member of group staff

--- ai_camera/views.py
+++ ai_camera/views.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import User, Group
 
 def index(request):
     user_is_member = request.user.groups.filter(name="camera_staff").exists()
-    return render(request, 'ai_camera/index.html')
+    return render(request, 'ai_camera/index.html', { 'user_is_member': user_is_member })
