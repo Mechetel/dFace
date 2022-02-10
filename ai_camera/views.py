5649c659f44ffc79c18ca88bdd2e42efc70ef70f
Mechetel <dima.homa5@gmail.com>
2022-02-10 17:42:16 +0200

    feat: remove unnecessary code

--- ai_camera/views.py
+++ ai_camera/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views import generic
@@ -6,34 +5,12 @@ from django.contrib.auth.models import User, Group
 from .models import Camera
 
 
+@method_decorator(login_required(login_url='/login/'), name='dispatch')
 class CameraListView(generic.ListView):
     model = Camera
     template_name = 'ai_camera/index.html'
 
-    def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
-        context = super().get_context_data(**kwargs)
-        # Add in the username
-        user_is_member = self.request.user.groups.filter(name="camera_staff").exists()
-        context['user_is_member'] = user_is_member
-        return context
-
-    @method_decorator(login_required(login_url='/login/'))
-    def dispatch(self, request, *args, **kwargs):
-        return super(CameraListView, self).dispatch(request, *args, **kwargs)
-
+@method_decorator(login_required(login_url='/login/'), name='dispatch')
 class CameraView(generic.DetailView):
     model = Camera
     template_name = 'ai_camera/camera.html'
-
-    def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
-        context = super().get_context_data(**kwargs)
-        # Add in the username
-        user_is_member = self.request.user.groups.filter(name="camera_staff").exists()
-        context['user_is_member'] = user_is_member
-        return context
-
-    @method_decorator(login_required(login_url='/login/'))
-    def dispatch(self, request, *args, **kwargs):
-        return super(CameraView, self).dispatch(request, *args, **kwargs)
