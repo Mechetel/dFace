21d3945b1577dc5bbfa08988dbb0cc9062bcf80d
Mechetel <dima.homa5@gmail.com>
2022-06-19 16:27:31 +0300

    feat: delete unnessesary + run not from docker

--- ai_camera/camera.py
+++ ai_camera/camera.py
@@ -4,8 +4,8 @@ import numpy as np
 
 
 class VideoCamera(object):
-    def __init__(self):
-        self.video = cv2.VideoCapture(0)
+    def __init__(self, url = 0):
+        self.video = cv2.VideoCapture(url)
 
     def __del__(self):
         cv2.destroyAllWindows()
@@ -15,19 +15,3 @@ class VideoCamera(object):
         frame_flip = cv2.flip(frame, 1)
         ret, frame = cv2.imencode('.jpg', frame_flip)
         return frame.tobytes()
-
-
-class IPWebCamera(object):
-    def __init__(self, url):
-        self.url = url
-        self.video = cv2.VideoCapture(url)
-
-    def __del__(self):
-        cv2.destroyAllWindows()
-
-    def get_frame(self):
-        ret, frame = self.video.read()
-        resize = cv2.resize(frame, (640, 480), interpolation = cv2.INTER_LINEAR)
-        ret, frame = cv2.imencode('.jpg', resize)
-        return frame.tobytes()
-
--- ai_camera/views.py
+++ ai_camera/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User, Group
 from django.shortcuts import render
 from django.http.response import StreamingHttpResponse
 from .models import Camera
-from ai_camera.camera import VideoCamera, IPWebCamera
+from ai_camera.camera import VideoCamera
 from django.views.decorators import gzip
 
 
@@ -24,25 +24,13 @@ class CameraListView(generic.ListView):
 @method_decorator(login_required(login_url='/login/'), name='dispatch')
 def ip_camera_view(request, id):
     camera = Camera.objects.get(id = id)
-    response = StreamingHttpResponse(gen(IPWebCamera(camera.url)),
+    response = StreamingHttpResponse(gen(VideoCamera(camera.url)),
                     content_type='multipart/x-mixed-replace; boundary=frame')
 
     return render(request, 'ai_camera/camera.html', response)
 
 @gzip.gzip_page
 def video_feed(request):
-
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
-        ip = request.META.get('REMOTE_ADDR')
-
-    print("==============")
-    print('rtsp://' + ip + '/0')
-    print("==============")
-
     return StreamingHttpResponse(gen(VideoCamera()),
                     content_type='multipart/x-mixed-replace; boundary=frame')
 
